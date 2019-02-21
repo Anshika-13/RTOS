0ab26ff4e28f6796562aaf7385270a38709f44b6
Anshika-13 <47719755+Anshika-13@users.noreply.github.com>
2019-02-21 14:00:30 +0530

    Update SERVER.c

--- ASSIGNMENT-1/EchoEngine/SERVER.c
+++ ASSIGNMENT-1/EchoEngine/SERVER.c
@@ -18,11 +18,6 @@ int main()
   
     // msgget creates a message queue and returns identifier 
     msgid = msgget(1000, 0666 | IPC_CREAT); //server will use this to receive
-    msgid2 = msgget(1011, 0666 | IPC_CREAT); //client will use this to receive
-    msgid3 = msgget(1012, 0666 | IPC_CREAT); //client will use this to receive
-    msgid4 = msgget(1013, 0666 | IPC_CREAT); //client will use this to receive
-    msgid5 = msgget(1014, 0666 | IPC_CREAT); //client will use this to receive
-    msgid6 = msgget(1015, 0666 | IPC_CREAT); //client will use this to receive
     message2.mesg_type = 1; 
 
     while (1) {
@@ -46,27 +41,8 @@ int main()
        
         printf("Character sent is : %s \n\n", message2.mesg_text); 
       
-      
-        if (message.cl == 1) {
-            printf("cl= %d\n", message.cl);
-            msgsnd(msgid2, &message2, sizeof(message2), 0); 
-        }
-        else if (message.cl == 2) {
-            printf("cl= %d\n", message.cl);
-            msgsnd(msgid3, &message2, sizeof(message2), 0);
-        }
-        else if (message.cl == 3) {
-            printf("cl= %d\n", message.cl);
-            msgsnd(msgid4, &message2, sizeof(message2), 0);
-        }
-        else if (message.cl == 4) {
-            printf("cl= %d\n", message.cl);
-            msgsnd(msgid5, &message2, sizeof(message2), 0);
-        }
-        else if (message.cl == 5) {
-            printf("cl= %d\n", message.cl);
-            msgsnd(msgid6, &message2, sizeof(message2), 0);
-        }
+        msgid2 = message.cl; //client will use this to receive
+        msgsnd(msgid2, &message2, sizeof(message2), 0);
 
         //emptying the character buffers
         memset(message.mesg_text, 0, sizeof(message.mesg_text));
