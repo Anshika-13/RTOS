15c250280456f5d0f81eebb53602f102772b1b42
Anshika-13 <47719755+Anshika-13@users.noreply.github.com>
2019-04-20 23:52:05 +0530

    Update Client.c

--- Assignment-3/Client.c
+++ Assignment-3/Client.c
@@ -1,1 +1,118 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h> 
+#include <sys/time.h>
 
+
+#include <errno.h>
+
+#include <pulse/simple.h>
+#include <pulse/error.h>
+#include <pulse/gccmacro.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define PORT 6000
+
+/* A simple routine calling UNIX write() in a loop */
+static ssize_t loop_write(int fd, const void*data, size_t size) {
+    ssize_t ret = 0;
+
+    while (size > 0) {
+        ssize_t r;
+
+        if ((r = write(fd, data, size)) < 0)
+            return r;
+
+        if (r == 0)
+            break;
+
+        ret += r;
+        data = (const uint8_t*) data + r;
+        size -= (size_t) r;
+    }
+
+    return ret;
+}
+
+int main(int argc, char const *argv[]) 
+{ 
+	struct sockaddr_in address; 
+	int sock = 0, valread; 
+	struct sockaddr_in serv_addr; 
+	// char *hello = "Hello from client"; 
+	// char hello[100];
+	// char buffer[1024] = {0}; 
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{ 
+		printf("\n Socket creation error \n"); 
+		return -1; 
+	} 
+
+	memset(&serv_addr, '0', sizeof(serv_addr)); 
+
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(PORT); 
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return -1; 
+	} 
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+	{ 
+		printf("\nConnection Failed \n"); 
+		return -1; 
+	} 
+
+    /* pulseaudio setup */
+    static const pa_sample_spec ss = {
+        .format = PA_SAMPLE_S16LE,
+        .rate = 44100,
+        .channels = 2
+    };
+    pa_simple *s = NULL;
+    int ret = 1;
+    int error;
+
+    int fd = open("rec_file",O_WRONLY);
+
+    /* Create the recording stream */
+    if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
+        fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
+        goto finish;
+    }
+
+    for (;;) {
+        uint8_t buf[1024];
+
+        /* Record some data ... */
+        if (pa_simple_read(s, buf, sizeof(buf), &error) < 0) {
+            fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
+            goto finish;
+        }
+
+        /* And write it into the socket to the server */
+        if (loop_write(sock, buf, sizeof(buf)) != sizeof(buf)) {
+            fprintf(stderr, __FILE__": write() failed: %s\n", strerror(errno));
+            goto finish;
+        }
+    }
+
+    ret = 0;
+
+finish:
+
+    if (s)
+        pa_simple_free(s);
+    close(fd);
+
+    return ret;
+}
